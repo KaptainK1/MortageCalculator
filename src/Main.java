44c334853d360ba65a68b63d20523c8e73b94f21
Dylan Hoffman <jkioicefire@gmail.com>
2018-07-03 04:58:50 -0500

    Version1.0.7

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,30 @@ import model.CarMortgage;
 import model.ConventionalMortgage;
 import model.Mortgage;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String args[]){
-        ConventionalMortgage mortgage = new ConventionalMortgage(4.625d,172000,360,6880,700, 248.35);
+
+        String userInput;
+        String array[];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter in the format InterestRate, PurchasePrice, Term, DownPayment, CreditScore, Escrow");
+        userInput=scanner.nextLine();
+        array=userInput.split(",");
+        // throw an exception when the array length doesn't equal 5
+        if (array.length != 6) {
+            throw new IllegalArgumentException("Not enough parameters or commas entered");
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+//        ConventionalMortgage mortgage = new ConventionalMortgage(4.625d,172000,360,6880,700, 248.35);
+        ConventionalMortgage mortgage = new ConventionalMortgage(Double.valueOf(array[0]), Integer.valueOf(array[1]), Integer.valueOf(array[2]),
+                                                                    Integer.valueOf(array[3]), Integer.valueOf(array[4]), Double.valueOf(array[5]));
         mortgage.calculateAmortization();
         mortgage.yearPrinciple(5);
         CarMortgage carMortgage = new CarMortgage(1.99,12000, 60,2000, 700, .05);
